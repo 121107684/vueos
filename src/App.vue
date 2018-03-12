1b86596f65248ef22494604b13af2280128cdc0c
baiyang <baiyang@bjyw-xiewenhao.hxsd.local>
2018-03-12 17:52:10 +0800

    creat

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="skin-blur-violate">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html, body {
+  height: 100%
+}
+#skin-blur-violate {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #fff;
+  min-height: 100%;
+}
+</style>
